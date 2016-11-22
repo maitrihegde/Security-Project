c15d822e4d55662da80e2bf7d1debc9aa7737a97
maitrihegde <maitri.hegde14@gmail.com>
2016-11-21 18:27:22 -0600

    commit

--- myProgram-standalone/myProgram.go
+++ myProgram-standalone/myProgram.go
@@ -1,3 +1,4 @@
+
 package main 
 
 import (
@@ -15,32 +16,39 @@ func main () {
 	if status != syscall.StatusOk {
 		efmt.Fprint(syscall.Stderr,"Error opening %v: %v\n", path, status)
         syscall.Exit(syscall.StatusOk)
-	}
+}
 
-
-//	data    := MyType { "foo", "bar" }
-//        fd, status := ethos.OpenDirectoryPath(path)
-//	data.Write(fd)
-//	data.WriteVar(path +"foobar")
-//        efmt.Fprint(syscall.Stderr,"this will print in the log")
-//        efmt.Println(data)
-//        efmt.Println(fd)
-
-        // I'm adding code for Box here
+      // I'm adding code for Box here
         mybox := MyType{}
-        mybox.w = 0.0
-        mybox.x = 1.1
-        mybox.y = 3.3
-        mybox.z = 4.4
+        mybox1 := MyType{}
+
+     // Assigning values for mybox    
+        mybox.x1 = 0.0
+        mybox.x2 = 1.1
+        mybox.y1 = 3.3
+        mybox.y2 = 4.4
+
         fd,status := ethos.OpenDirectoryPath(path)
         mybox.Write(fd)
         mybox.WriteVar(path)
-        efmt.Fprint(syscall.Stderr,"this will print in the log")
-        efmt.Println(mybox)
-     //   efmt.Println(fd)
-        a := (1.1-0.0)*(1.1-0.0)
-        b := (4.4-3.3)*(4.4-3.3)
-        efmt.Println("The distance between the two coordinates are",math.Sqrt(a+b))
-        efmt.Println("You have successed completed the program")
+        efmt.Fprint(syscall.Stderr,"\n This will print in the log\n")
+        efmt.Println("\n The coordinates for the first instance are  ",mybox)
+
+     // Computing distnace between the two coordinates
+        a := (mybox.x2-mybox.x1)*(mybox.x2-mybox.x1)
+        b := (mybox.y2-mybox.y1)*(mybox.y2-mybox.y1)
+        efmt.Println("The distance between the two coordinates is ",math.Sqrt(a+b))
+
+ 
+     // Computing Square root and writing into the new instance 'mybox1'
+        mybox1.x1 = math.Sqrt(mybox.x1)
+        mybox1.y1 = math.Sqrt(mybox.y1)
+        mybox1.x2 = math.Sqrt(mybox.x2)
+        mybox1.y2 = math.Sqrt(mybox.y2)
+        mybox1.Write(fd)
+        mybox1.WriteVar(path)
+        
+        efmt.Println("\n The coordinates for the new instance are ",mybox1)
+        efmt.Println("\n You have successed completed the program\n")
   
 }
